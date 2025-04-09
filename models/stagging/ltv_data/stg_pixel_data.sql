004f802b69d11e0705738cf43e5b06fe0cec6c12
dapira28@gmail.com <dapira28@gmail.com>
2025-04-09 22:23:08 +0000

    stg pixed data

--- models/stagging/ltv_data/stg_pixel_data.sql
+++ models/stagging/ltv_data/stg_pixel_data.sql
@@ -5,4 +5,5 @@ SELECT
     purchase,
     revenue/1e6 AS revenue,
     CONVERT_TIMEZONE('US/Pacific', 'UTC', timestamp_max) timestamp_max
+    CONVERT_TIMEZONE('US/Pacific', 'UTC', timestamp_min) timestamp_min
 FROM {{ source("zan_zmetrics", "agg_visit_campaign_pixel") }}
