576751a992bc61fa89a0b940f715713e1f316e4c
Ale Miranda <dapira28@gmail.com>
2025-04-09 23:53:36 +0000

    update stg model

--- models/stagging/ltv_data/stg_pixel_data.sql
+++ models/stagging/ltv_data/stg_pixel_data.sql
@@ -4,6 +4,6 @@ SELECT
     campaign_tracking_id,
     purchase,
     revenue/1e6 AS revenue,
-    CONVERT_TIMEZONE('US/Pacific', 'UTC', timestamp_max) timestamp_max
+    CONVERT_TIMEZONE('US/Pacific', 'UTC', timestamp_max) timestamp_max,
     CONVERT_TIMEZONE('US/Pacific', 'UTC', timestamp_min) timestamp_min
 FROM {{ source("zan_zmetrics", "agg_visit_campaign_pixel") }}
