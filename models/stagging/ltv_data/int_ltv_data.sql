f411fad4c2538afa62a554a818a3c1ffe92f6600
dapira28@gmail.com <dapira28@gmail.com>
2025-04-09 00:55:56 +0000

    update lvt data final

---
+++ models/stagging/ltv_data/int_ltv_data.sql
@@ -0,0 +1,19 @@
+SELECT
+c.visit_date,
+c.property_name,
+c.publisher_visit_id,
+c.publisher_id,
+c.publisher_name,
+c.utm_source,
+c.utm_term,
+c.utm_medium,
+c.utm_content,
+SUM(c.campaign_impressions) AS campaign_impressions,
+SUM(c.campaign_conversions) AS campaign_conversions,
+SUM(c.campaign_clicked) AS campaign_clicks,
+SUM(c.purchase_count) AS purchase_count,
+SUM(c.pixel_revenue) AS pixel_revenue
+FROM {{ ref('stg_campaign_event_data') }} c
+INNER JOIN {{ ref('stg_campaign_with_purchases') }} fc ON c.campaign_id = fc.campaign_id
+GROUP BY ALL
+QUALIFY ROW_NUMBER() OVER (partition by c.publisher_visit_id ORDER BY c.visit_date DESC) = 1
---
+++ models/stagging/ltv_data/stg_campaign_with_purchases.sql
@@ -0,0 +1,13 @@
+SELECT
+c.campaign_id,
+c.campaign_name,
+c.advertiser_name,
+SUM(c.campaign_impressions) AS campaign_impressions,
+SUM(c.campaign_conversions) AS campaign_conversions,
+SUM(c.campaign_clicked) AS campaign_clicked,
+SUM(c.purchase_count) AS purchase_count,
+SUM(c.pixel_revenue) AS pixel_revenue
+FROM {{ ref('stg_pixel_data') }} c
+GROUP BY ALL
+HAVING SUM(c.purchase_count) >0
+ORDER BY 7 DESC
